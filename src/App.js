5c633c7b76e83e0f6570afb306184756afe2ab25
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-05-29 23:00:50 +0600

    Smart contract redeployed to fix bug

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
   const [account, setAccount] = useState();
   const [contactList, setContactList] = useState();
   const [contactsName, setContactsName] = useState([]);
+  const [contactsAge, setContactsAge] = useState([]);
   const [nameValue, setNameValue] = useState('');
+  const [ageValue, setAgeValue] = useState(0);
   let CONTACT_ABI = NameStorageContract.abi;
-  let CONTACT_ADDRESS = "0xc7e8eBD5cafA964e244579DC88aDE8C93012B9fc"
+  let CONTACT_ADDRESS = "0x716E6117622eB54Aa287cc02565a9D3897C71f8B"
   useEffect(() => {
     async function load() {
       const web3 = new Web3(Web3.givenProvider || '');
@@ -21,22 +23,40 @@ function App() {
       // Then we get total number of contacts for iteration
       const name = await contactList.methods.name().call();
       setContactsName(name);
+      const age = await contactList.methods.age().call();
+      setContactsAge(age);
     }
     
     load();
   }, []);
+
   const setUserName = async () => {
     
     await contactList.methods.setName(nameValue).send({ from: account });
+    
     const newName = await contactList.methods.name().call();
-      setContactsName(newName);
+    
+    setContactsName(newName);
+    
     console.log("Name set to: " + nameValue + " " + newName);
+    
   }
+
+  const setUserAge = async () =>{
+    await contactList.methods.setAge(ageValue).send({ from: account });
+    const newAge = await contactList.methods.age().call();
+    setContactsAge(newAge);
+    console.log("newAge set to: " + newAge + " " + newAge);
+  }
+
   return (
     <div>
-    <h1>Your account is: {contactsName}</h1> 
-    <input type="text" onChange={(e)=>setNameValue(e.target.value)}></input>
-    <button onClick={()=>setUserName()}>SetName</button>
+    <h1>Your name is: {contactsName}</h1> 
+    <h1>Your age is: {contactsAge}</h1> 
+    <input type="text" onChange={(e)=>setNameValue(e.target.value)}/>{" "}
+    <button onClick={()=>setUserName()}>SetName</button>{" "}
+    <input type="number" onChange={(e)=>setAgeValue(e.target.value)}/>{" "}
+    <button onClick={()=>setUserAge()}>SetAge</button>
     </div>
   );
 }
